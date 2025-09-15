e6cec8b7c37c844fb28589f9f61f10c74dac5c78
vaibhav kulkarni <vaibhavkulkarni614@gmail.com>
2025-09-15 22:59:49 +0530

    Add files via upload

---
+++ program10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void Display()
+{
+  printf("hello\n");
+  printf("hello\n");
+  printf("hello\n");
+  printf("hello\n");
+}
+
+int main()
+{
+  Display();
+
+  return 0;
+}
---
+++ program11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void Display()
+{
+  int icnt = 0;
+  for (icnt = 0; icnt < 4; icnt++)
+  {
+    printf("hello\n");
+  }
+}
+
+int main()
+{
+  Display();
+
+  return 0;
+}
